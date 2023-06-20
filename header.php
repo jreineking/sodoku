3ac1a89712b2eb7c5e0818392e46d27ad84a81f0
jreineking <137101887+jreineking@users.noreply.github.com>
2023-06-20 12:45:33 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sodoku Matemático</title>
+    <link rel="stylesheet" type="text/css" href="estilo_header.css">
+</head>
+<header class="header">
+    <div class="logo-header">
+        <img src="logo.png" alt="" width="50px">
+    </div>
+    <nav class="nav-menu">
+        <ul>
+            <li><a href="sumar.php">Adición</a></li>
+            <li><a href="restar.php">Sustracción</a></li>
+            <li><a href="sumar.php">Multiplicación</a></li>
+            <li><a href="sumar.php">División</a></li>
+            <li><a href="sumar.php">Mixto</a></li>
+        </ul>
+    </nav>
+</header>
+<body>
+</body>
+</html>
---
+++ paso.php
@@ -0,0 +1,57 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sodoku Matemática</title>
+    <link rel="stylesheet" href="estilo_sumar.css">
+</head>
+<header>
+    <?php include "header.php";?>
+</header>
+<body>
+    <div class="contenedor">
+        <h1>SUMAR</h1>
+        <table>
+            <tr>
+                <td>26</td>
+                <td>+</td>
+                <td></td>
+                <td>=</td>
+                <td>42</td>
+            </tr>
+            <tr>
+                <td>+</td>
+                <td></td>
+                <td>+</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>+</td>
+                <td></td>
+                <td>=</td>
+                <td>10</td>
+            </tr>
+            <tr>
+                <td>=</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>35</td>
+                <td>+</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>    
+</body>
+</html>
